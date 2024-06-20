41ab53ee6a761610e655ffdbc84dac4c3d7365f3
Amr <Amr.rimawi@iqvia.com>
2024-06-20 19:10:23 +0300

    cleaning

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        primary: '#c1a53b',
+        secondary: {
+          100: '#547d59',
+          200: '#3f6143',
+        }
+      }
     },
   },
   plugins: [],
