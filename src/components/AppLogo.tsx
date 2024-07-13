7fd6a47d60e8504f1c5c33adae855c226b502ca7
Amr <Amr.rimawi@iqvia.com>
2024-07-13 10:23:46 +0300

    sosials

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import Image from "next/image";
 
 export const AppLogo = () => (
-    <Image className="m-5" width={50} height={100} src="/imgs/rawas-flag.svg" alt="Rawas"/>
+    <Image className="m-5" width={50} height={100} src="@/imgs/rawas-flag.svg" alt="Rawas"/>
 );
