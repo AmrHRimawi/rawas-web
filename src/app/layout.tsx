c26e3e9bfac776dbc8295700ff9e2449d2f10c52
Amr <Amr.hrimawi@gmail.com>
2025-05-17 19:32:42 +0300

    Refactor app metadata and update title structure for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,20 @@
+import React from "react";
 import {Tajawal} from "next/font/google";
 import "./globals.css";
 import AppNavBar from "@/components/AppNavBar";
 import {NextUIProvider} from "@nextui-org/react";
 import AppFooter from "@/components/AppFooter";
-import {appMetadata, localBusinessJsonLd, title} from "@/app/appMetadata";
+import {appMetadata, localBusinessJsonLd, title} from "@/utils/appMetadata";
 
 const font = Tajawal({subsets: ['arabic', 'latin'], weight: ['200', '300', '400', '500', '700', '800', '900']});
 export const metadata = appMetadata;
 
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="ar" className="lite text-foreground bg-background">
         <head>
             {/* Inject LocalBusiness schema */}
-            <script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{__html: JSON.stringify(localBusinessJsonLd)}}
-            />
+            <script type="application/ld+json" dangerouslySetInnerHTML={{__html: JSON.stringify(localBusinessJsonLd)}}/>
             <title>{title}</title>
         </head>
         <body className={font.className}>
