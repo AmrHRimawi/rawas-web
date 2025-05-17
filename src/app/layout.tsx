dded0542db8b62394af189eedcbe80a4d4e4a550
Amr <Amr.hrimawi@gmail.com>
2025-05-17 20:50:01 +0300

    Refactor layout component to streamline metadata handling and improve SEO structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import AppNavBar from "@/components/AppNavBar";
 import {NextUIProvider} from "@nextui-org/react";
 import AppFooter from "@/components/AppFooter";
-import {appMetadata, localBusinessJsonLd, title} from "@/utils/MetadataUtil";
+import {appMetadata, localBusinessJsonLd} from "@/utils/MetadataUtil";
 
 const font = Tajawal({subsets: ['arabic', 'latin'], weight: ['200', '300', '400', '500', '700', '800', '900']});
 export const metadata = appMetadata;
@@ -12,17 +12,14 @@ export const metadata = appMetadata;
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="ar" className="lite text-foreground bg-background">
-        <head>
-            {/* Inject LocalBusiness schema */}
-            <script type="application/ld+json" dangerouslySetInnerHTML={{__html: JSON.stringify(localBusinessJsonLd)}}/>
-            <title>{title}</title>
-        </head>
         <body className={font.className}>
         <NextUIProvider>
             <AppNavBar/>
             {children}
             <AppFooter/>
         </NextUIProvider>
+        {/* Inject LocalBusiness schema */}
+        <script type="application/ld+json" dangerouslySetInnerHTML={{__html: JSON.stringify(localBusinessJsonLd)}}/>
         </body>
         </html>
     );
