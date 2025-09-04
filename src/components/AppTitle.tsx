ff404eaedc3d35c6521e9fdeabec1b8011ff4770
Amr Rimawi <Amr.hrimawi@gmail.com>
2025-09-04 18:13:28 +0300

    Refactor AppTitle components for improved styling and visual effects

--- src/components/AppTitle.tsx
+++ src/components/AppTitle.tsx
@@ -9,11 +9,11 @@ interface AppTitleProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTM
 
 const AppTitle: React.FC<AppTitleProps> = ({prefix = "", text = "", className, ...props}) => {
     return (<MotionEndDiv>
-        <h2 className={"text-2xl md:text-4xl lg:text-6xl font-bold " + className} {...props}>
+        <h2 className={"relative text-2xl md:text-4xl lg:text-6xl font-bold after:block after:h-[3px] after:w-28 after:bg-secondary after:mt-2 after:rounded-full after:transition-all after:duration-500 hover:after:w-36 " + className} {...props}>
             <span className="w-28 pt-2 bg-secondary text-secondary border-b-4 border-secondary"> . </span>
             <span className="w-4 ps-8 pt-2 bg-primary text-primary-foreground border-b-4 border-primary">{prefix}</span>
             <span className="border-b-4 border-secondary text-primary">{text}</span>
-            <span className="border-b-4 border-secondary border-dotted text-primary-foreground">..</span>
+            <span className="border-b-4 border-secondary border-dotted text-primary-foreground text-opacity-0">..</span>
         </h2>
     </MotionEndDiv>)
 }
