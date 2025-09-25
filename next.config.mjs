11259d960a72ec2e12e68aa027932a144f921d3e
Amr Rimawi <Amr.hrimawi@gmail.com>
2025-09-25 19:51:35 +0300

    Enables static export of the Next.js application.
    Configures the Next.js application to be exported as a static site.
    This involves enabling the `output: "export"` option in the Next.js configuration file.
    This makes the application suitable for deployment on static hosting platforms.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",
-    trailingSlash: true,
+    output: "export",
+    // trailingSlash: true,
     distDir: "out",
     // basePath: "/rawas-web",
     // assetPrefix: "/rawas-web/",
